b2f9a260e0d8c6150c33d0a85d23f28e5d514a16
Partenio <diogvargas@gmail.com>
2021-11-07 15:50:02 -0400

    changes in Temperature calculations. more to be added

--- Temperatura.py
+++ Temperatura.py
@@ -1,5 +1,8 @@
 
 
 def temp_calculation(temp_hot_box, temp_cold_box):
-    output = temp_hot_box - temp_cold_box
+    temp_in = temp_hot_box - temp_cold_box
+    q = 1
+    a = 0.15*0.15
+    output = q/(a*temp_in)
     return output
